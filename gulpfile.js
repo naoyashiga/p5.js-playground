1ad16e3c6b64fceae857c68a9c2669a714799f70
naoyashiga <naoyashiga0@gmail.com>
2016-02-24 21:53:37 +0900

    setup dev env

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,19 @@
-
 var gulp = require('gulp'), 
     sass = require('gulp-ruby-sass') 
     notify = require("gulp-notify") 
     webserver = require('gulp-webserver');
 
-
 gulp.task('server', function() {
   gulp.src('./')
     .pipe(webserver({
       livereload: true,
-      port: 8001,
-      fallback: 'index.html',
       open: true
     }));
 });
 
 // Rerun the task when a file changes
  gulp.task('watch', function() {
-     gulp.watch('script.js', ['']); 
+     gulp.watch('sketches/**/*.js', ['']); 
 });
 
   gulp.task('default', ['watch','server']);
