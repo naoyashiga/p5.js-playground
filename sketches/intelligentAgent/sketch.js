1ffe9f212ba63dd2e1ede70519824f5a8801b5df
naoyashiga <naoyashiga0@gmail.com>
2016-02-25 20:58:10 +0900

    draw simple line

--- sketches/intelligentAgent/sketch.js
+++ sketches/intelligentAgent/sketch.js
@@ -1,30 +1,98 @@
-var yoff = 0.0;
+var NORTH = 0,
+EAST = 1,
+SOUTH = 2,
+WEST = 3,
+posX, posY,
+posXcross, posYcross,
+direction = SOUTH,
+angleCount = 7,
+angle = getRandomAngle(direction),
+stepSize = 30,
+minLength = 10;
+
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  smooth();
+  background(0);
+
+  posX = Math.floor(getRandomNum(0, width));
+  posY = 5;
+  posXcross = posX;
+  posYcross = posY;
 }
 
 function draw() {
-	background(51);
+	// background(51);
+	for (var i = 0; i < 1; i++) {
+		strokeWeight(1);
+		stroke(200);
 
-	fill(255);
+		point(posX, posY);
 
-	beginShape();
+		posX += cos(radians(angle)) * stepSize;
+		posY += sin(radians(angle)) * stepSize;
 
-	// var xoff = 0;
-	var xoff = yoff;
+		var reachedBorder = false;
 
-	for (var x = 0; x <= width; x += 10) {
-		var y = map(noise(xoff, yoff), 0, 1, height / 2, height / 1.2);
-		// var y = map(noise(xoff), 0, 1, height / 2, height / 1.2);
+		if (posY <= 5) {
+			direction = SOUTH;
+			reachedBorder = true;
+		}
+		else if (posX >= width - 5) {
+			direction = WEST;
+			reachedBorder = true;
+		} 
+		else if (posY >= height - 5) {
+			direction = NORTH;
+			reachedBorder = true;
+		} 
+		else if (posX <= 5) {
+			direction = EAST;
+			reachedBorder = true;
+		} 
 
-		vertex(x ,y);
+		// crossing
+		var px = posX;
+		var py = posY;
 
-		xoff += 0.05;
+		if (get(px, py) != color(0) || reachedBorder) {
+			angle = getRandomAngle(direction);
+
+			var distance = dist(posX, posY, posXcross, posYcross);
+
+
+			if(distance >= minLength) {
+				strokeWeight(1);
+				stroke(200);
+				line(posX, posY, posXcross, posYcross);
+			}
+
+			posXcross = posX;
+			posYcross = posY;
+		}
 	}
+}
 
-	yoff += 0.01;
-	vertex(width, height);
-	vertex(0, height);
-	endShape(CLOSE);
+function getRandomAngle(theDirection) {
+	var a = Math.floor(getRandomNum(-angleCount, angleCount)) + 0.5 * 90.0 / angleCount;
+
+	// console.log(a);
+
+	switch(theDirection) {
+		case NORTH:
+			return a - 90;
+		case EAST:
+			return a;
+		case SOUTH:
+			return a + 90;
+		case WEST:
+			return a + 180;
+		default:
+			return 0;
+	}
+}
+
+function getRandomNum(min, max) {
+	return Math.random() * (max - min) + min;
 }
