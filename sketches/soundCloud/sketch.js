01c549675b850bc5c5f05692490d80ac1c3e050b
naoyashiga <naoyashiga0@gmail.com>
2016-03-06 20:12:39 +0900

    get stream data from soundcloud

--- sketches/soundCloud/sketch.js
+++ sketches/soundCloud/sketch.js
@@ -1,13 +1,36 @@
+var TRACK_URL = "https://soundcloud.com/jerbeary/mrsaxobeat";
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+var visualize = function(track) {
+	var streamURL = track.stream_url + "?client_id=" + CLIENT_ID;
 
-  console.log(CLIENT_ID);
-}
+	var sketch = function(p) {
 
-function draw() {
-	background(51);
+		var song;
+		
+		p.preload = function(){
+			song = p.loadSound(streamURL);
+		}
 
-	fill(255);
+		p.setup = function() {
+			p.createCanvas(window.innerWidth, window.innerHeight);
+		};
 
-}
+		p.draw = function() {
+			p.background(51);
+			p.fill(255);
+		};
+	};
+
+	var myp5 = new p5(sketch);
+};
+
+SC.initialize({
+	client_id: CLIENT_ID
+});
+
+SC.resolve(TRACK_URL).then(function(track){
+	visualize(track);
+
+}).catch(function(error){
+	console.log(error);
+});
