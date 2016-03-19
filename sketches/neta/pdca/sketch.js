4b14ff4d943bf3ebb765d42b51ff88ee2de322a5
naoyashiga <naoyashiga0@gmail.com>
2016-03-19 12:37:46 +0900

    pdcaをまわす

--- sketches/neta/pdca/sketch.js
+++ sketches/neta/pdca/sketch.js
@@ -1,26 +1,65 @@
-var tSize = 32;
+var sketch = function($p) {
 
-var corner;
+	var corner;
+	var angle = 0;
 
-var direction = 30;
+	var variables = {
+		rotateSpeed: 0.01,
+		direction: 30,
+		tSize: 32
+	}
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+	$p.setVariables = function(_v) {
+		variables = _v;
+	}
 
-  textSize(tSize);
+	$p.setup = function() {
+		$p.createCanvas(window.innerWidth, window.innerHeight);
+		$p.textSize(variables.tSize);
 
-  corner = createVector(direction, direction);
-}
+		corner = $p.createVector(variables.direction, variables.direction);
 
-function draw() {
-	background(0);
+		$p.textSize(variables.tSize);
+	};
 
-	fill(255);
+	$p.draw = function() {
+		$p.background(0);
 
-	translate(width / 2, height / 2);
-	text("P", -corner.x - tSize / 2, -corner.y + tSize / 2);
-	text("D", corner.x - tSize / 2, -corner.y + tSize / 2);
-	text("C", corner.x - tSize / 2, corner.y + tSize / 2);
-	text("A", -corner.x - tSize / 2, corner.y + tSize / 2);
+		$p.fill(255);
 
-}
+		angle += variables.rotateSpeed;
+
+		corner = $p.createVector(variables.direction, variables.direction);
+		$p.textSize(variables.tSize);
+
+		$p.translate($p.width / 2, $p.height / 2);
+
+		$p.rotate(angle);
+
+		$p.text("P", -corner.x - variables.tSize / 2, -corner.y + variables.tSize / 2);
+		$p.text("D", corner.x - variables.tSize / 2, -corner.y + variables.tSize / 2);
+		$p.text("C", corner.x - variables.tSize / 2, corner.y + variables.tSize / 2);
+		$p.text("A", -corner.x - variables.tSize / 2, corner.y + variables.tSize / 2);
+	};
+};
+
+var Variables = function() {
+	this.rotateSpeed = 0.01;
+	this.direction = 100;
+	this.tSize = 200;
+};
+
+
+window.onload = function() {
+	var myp5 = new p5(sketch);
+	var variables = new Variables();
+
+	myp5.setVariables(variables);
+
+	var gui = new dat.GUI();
+
+	gui.add(variables, 'rotateSpeed', 0, 0.2);
+	gui.add(variables, 'direction', 1, 300);
+	gui.add(variables, 'tSize', 1, 300);
+
+};
