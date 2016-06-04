ab220f7f2c52fa9c6f32405255013f968ebf51ed
naoyashiga <naoyashiga0@gmail.com>
2016-06-04 15:06:13 +0900

    draw line

--- sketches/generative/vertex/js/components/Visualization.js
+++ sketches/generative/vertex/js/components/Visualization.js
@@ -13,34 +13,66 @@ const sketch = (p) => {
 
   const density = 30;
 
+  var center = {x: 0, y: 0};
+  var stepSize = 2;
+
+  var vertexLength = 15;
+  var px = [];
+  var py = [];
+
+  var debug = 1;
+
   p.setup = function() {
-    let colorTheme = getRandomArrayIndex(ColorPalette);
-    let bgColorIndex = getRandomArrayIndex(ColorPalette[colorTheme]);
     let xoff = 1;
     let yoff = 1;
 
-    var px = [];
-    var py = [];
 
     p.createCanvas(p.windowWidth, p.windowHeight);
+
+    center.x = p.windowWidth / 2;
+    // center.y = p.windowHeight / 2;
+    center.y = 0;
+
     p.background("#ffffff");
 
-    p.noLoop();
+    // p.noLoop();
 
-    p.strokeWeight(1);
+    p.strokeWeight(0.75);
     p.stroke("#000000");
     p.noFill();
 
 
-    var stepSize = 40;
 
-    var vertexLength = 10;
+    var radius = 150 * p.random(0.5, 5.0);
+    var angle = p.random(p.PI);
+
+    radius = p.windowWidth;
+    angle = 0;
+
+
+
+    var x1 = p.cos(angle) * radius;
+    var y1 = p.sin(angle) * radius;
+    var x2 = p.cos(angle - p.PI) * radius;
+    var y2 = p.sin(angle - p.PI) * radius;
 
     for(var i = 0; i < vertexLength; i++) {
+      var tx = p.lerp(x1, x2, i / vertexLength);
+      var ty = p.lerp(y1, y2, i / vertexLength);
 
-      px.push(100 * i);
-      py.push(p.windowHeight / 2);
+      px.push(tx);
+      py.push(ty);
     }
+
+  }
+
+  p.draw = function () {
+    if(p.mouseX != 0 || p.mouseY != 0) {
+      center.x += (p.mouseX - center.x) * 0.01;
+      center.y += (p.mouseY - center.y) * 0.01;
+      // center.y += (p.windowHeight - center.y) * 0.01;
+    }
+
 
     for(var i = 0; i < vertexLength; i++) {
 
@@ -48,27 +80,45 @@ const sketch = (p) => {
       py[i] += p.random(-stepSize,stepSize);
 
       // p.ellipse(px[i], py[i], 10 , 10);
-      console.log(px[i], py[i]);
+      // console.log(px[i], py[i]);
     }
 
     p.beginShape();
-    // p.vertex(px[vertexLength - 1], py[vertexLength - 1]);
+    p.curveVertex(px[0] + center.x, py[0] + center.y);
 
     for(var i = 0; i < vertexLength; i++) {
-      p.vertex(px[i], py[i]);
-
+      p.curveVertex(px[i] + center.x, py[i] + center.y);
     }
 
-    // p.vertex(px[0], py[0]);
-    // p.vertex(px[1], py[1]);
+    p.curveVertex(px[vertexLength - 1] + center.x, py[vertexLength - 1] + center.y);
 
     p.endShape();
+
   }
 
-  // p.mousePressed = function() {
-  //   p.clear();
-  //   p.setup();
-  // }
+  p.keyPressed = function() {
+
+    console.log(p.keyCode);
+    if(p.keyCode == p.UP_ARROW) {
+
+      if(debug) {
+        debug = 0;
+        p.noLoop();
+        console.log("no loop");
+
+      } else {
+        debug = 1;
+        console.log("loop");
+        p.loop();
+      }
+    }
+
+  }
+
+  p.mousePressed = function() {
+    p.clear();
+    p.setup();
+  }
 
   p.windowResized = function() {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
